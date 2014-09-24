7c752e93ce78a8edb74c448264ebb408baabb181
stefanRitter <stefan@stefanritter.com>
2014-09-24 10:07:12 +0100

    grid layout

--- ngFamous/js/app.js
+++ ngFamous/js/app.js
@@ -23,7 +23,17 @@ angular.module('famousUniversity')
   });
 
 angular.module('famousUniversity')
-  .controller('Lesson2Ctrl', function($scope){
-      $scope.myGridLayoutOptions = {
-        dimensions: [2, 3]
-      };
+  .controller('Lesson2Ctrl', function($scope) {
+    $scope.myGridLayoutOptions = {
+      dimensions: [2, 3]
+    };
+    
+    $scope.list = [
+      {content:"hello", bgColor: "#b58900"},
+      {content:"world", bgColor: "#cb4b16"},
+      {content: "famous", bgColor: "#dc322f"},
+      {content: "angular", bgColor: "#d33682"},
+      {content: "is", bgColor: "#6c71c4"},
+      {content: "cool!", bgColor: "#268bd2"}
+    ];
+  });

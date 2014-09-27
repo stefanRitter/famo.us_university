cf6ef8b46af65bf0dd90b23705d76f90d1c159fb
stefanRitter <stefan@stefanritter.com>
2014-09-27 12:57:17 +0100

    restart timbre

--- timbre/src/main.js
+++ timbre/src/main.js
@@ -1,10 +1,5 @@
 define(function(require, exports, module) {
 	var Engine  = require('famous/core/Engine');
 
-  var AppView = require('views/AppView');
-
 	var mainContext = Engine.createContext();
-  var appView = new AppView();
-
-  mainContext.add(appView);
 });

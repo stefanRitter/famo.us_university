7a9b9784d9b2c6105c872a472817b693eaf4f950
stefanRitter <stefan@stefanritter.com>
2014-09-27 13:21:50 +0100

    align

--- timbre/src/main.js
+++ timbre/src/main.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
 	var Engine  = require('famous/core/Engine');
 
 	var mainContext = Engine.createContext();
--- timbre/src/views/EmptyView.js
+++ timbre/src/views/EmptyView.js
@@ -1,17 +1,17 @@
-define(function(require, exports, module) {
-    var View          = require('famous/core/View');
-    var Surface       = require('famous/core/Surface');
-    var Transform     = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+define(function (require, exports, module) {
+  var View          = require('famous/core/View');
+  var Surface       = require('famous/core/Surface');
+  var Transform     = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
 
-    function EmptyView() {
-        View.apply(this, arguments);
-    }
+  function EmptyView() {
+    View.apply(this, arguments);
+  }
 
-    EmptyView.prototype = Object.create(View.prototype);
-    EmptyView.prototype.constructor = EmptyView;
+  EmptyView.prototype = Object.create(View.prototype);
+  EmptyView.prototype.constructor = EmptyView;
 
-    EmptyView.DEFAULT_OPTIONS = {};
+  EmptyView.DEFAULT_OPTIONS = {};
 
-    module.exports = EmptyView;
+  module.exports = EmptyView;
 });

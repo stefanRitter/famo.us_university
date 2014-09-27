9eb14a1e188712c6394d92a5140bb7a9e9a481fd
stefanRitter <stefan@stefanritter.com>
2014-09-27 17:26:15 +0100

    setup main views

--- timbre/src/main.js
+++ timbre/src/main.js
@@ -1,5 +1,9 @@
 define(function (require, exports, module) {
 	var Engine  = require('famous/core/Engine');
+  var AppView = require('views/AppView');
 
 	var mainContext = Engine.createContext();
+  var appView = new AppView();
+
+  mainContext.add(appView);
 });
---
+++ timbre/src/views/AppView.js
@@ -0,0 +1,30 @@
+define(function (require, exports, module) {
+  var View          = require('famous/core/View');
+  var Surface       = require('famous/core/Surface');
+  var Transform     = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
+
+  var PageView =  require('views/PageView');
+
+  function AppView() {
+    View.apply(this, arguments);
+
+    _createPageView.call(this);
+  }
+
+  AppView.prototype = Object.create(View.prototype);
+  AppView.prototype.constructor = AppView;
+
+  AppView.DEFAULT_OPTIONS = {};
+
+
+  function _createPageView() {
+    this.pageView = new PageView();
+    this.pageModifier = new StateModifier();
+
+    this.add(this.pageModifier).add(this.pageView);
+  }
+
+
+  module.exports = AppView;
+});
